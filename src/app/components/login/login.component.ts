1b4babf272db698caadb01b22dc6748646b579b1
anamsilva1981 <ana.msilva@hotmail.com>
2025-10-06 09:10:20 -0300

    feat: add registration link to login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,12 +4,13 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardModule, InputTextModule, FormsModule, PasswordModule, ButtonModule],
+  imports: [CardModule, InputTextModule, FormsModule, PasswordModule, ButtonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
